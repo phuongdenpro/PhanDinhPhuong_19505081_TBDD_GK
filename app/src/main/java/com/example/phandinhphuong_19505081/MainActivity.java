fba04410ccb1c56f82a5ce668f6085ed1adec8ca
unknown <phuongdenpro@gmail.com>
2022-03-21 17:05:35 +0700

    cau3: finish

--- app/src/main/java/com/example/phandinhphuong_19505081/MainActivity.java
+++ app/src/main/java/com/example/phandinhphuong_19505081/MainActivity.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     ListView lvPr;
     ProductsAdapter adt;
-ArrayList<Products> arrayList;
+    ArrayList<Products> arrayList;
 
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
@@ -18,8 +18,9 @@ ArrayList<Products> arrayList;
         arrayList = new ArrayList<>();
         arrayList.add(new Products(R.drawable.bione_removebg_preview,"Red Bull One","$350","$449"));
         arrayList.add(new Products(R.drawable.bifour__removebg_preview,"Blue One","$840","$950"));
+
         adt = new ProductsAdapter(this,R.layout.listview_item,arrayList);
-         lvPr.setAdapter(adt);
+        lvPr.setAdapter(adt);
     }
 
 }
